365f30d6b5b3760ea8afe05618b75de7c6d416c9
setalosas <setalosas@gmail.com>
2021-04-17 01:09:02 +0200

    Complete refactoring in progress.

--- js/playground-esm.js
+++ js/playground-esm.js
@@ -10,15 +10,17 @@ const {Corelib, Visualizer} = pgIm
 
 const {s_a, undef, isFun, isNum, getRnd, hashOfString, ascii} = Corelib
 const {wassert, weject} = Corelib.Debug
-const {schedule, adelay, NoW, since, startEndThrottle} = Corelib.Tardis
+const {schedule, adelay, NoW, since, startEndThrottle, post} = Corelib.Tardis
 const {createSpectrumVisualizer} = Visualizer
 const {BroadcastChannel} = window
 
 const {BeeFX, BPM, Players, Sources, StateManager, StageManager, createUI} = pgIm
 
-const createPlayground = root => {
+const createPlayground = async root => {
   const {waCtx, ui} = root
-  const {newFx, namesDb, connectArr} = BeeFX(waCtx)
+  const {newFx, namesDb, connectArr, onReady: onBeeReady} = BeeFX(waCtx)
+  await onBeeReady()
+  
   ui.configNames(namesDb)
   
   //const dummyFx = waCtx.createGain()
@@ -26,7 +28,7 @@ const createPlayground = root => {
   const stageMan = StageManager.createStageManager(root)
   const {getEndRatios, iterateStages, getFilteredStages, getStage} = stageMan
 
-  const dis = {
+  const pg = {
     stageMan, //+ PFUJ de sourcesnek kell es fxpaneluinak is
     bpmAuditor: undef,
     bpmTransformer: newFx('fx_bpmTransformer'),
@@ -40,8 +42,8 @@ const createPlayground = root => {
     fingerPrint: getRnd(100000, 999999)
   }
   
-  const sources = dis.sources = Sources.createSources(dis, root)
-  const players = dis.players = Players.extendWithPlayers(dis, root)
+  const sources = pg.sources = Sources.createSources(pg, root)
+  const players = pg.players = Players.extendWithPlayers(pg, root)
   const {radio} = players
   
   //8#a6c Compose & decompose - they won't touch sources, starting from stInput of stages
@@ -83,48 +85,48 @@ const createPlayground = root => {
         }
       }
     })
-    wassert(unconnected || dis.graphMode === 'parallel')
+    wassert(unconnected || pg.graphMode === 'parallel')
     void unconnected?.connect(output)
   } */
 
   //8#a66 ----------- Change core playground Fxs -----------
     
-  dis.changeFx = (stageId, ix, type) => stageMan.changeFx({stageId, ix, type})
+  pg.changeFx = (stageId, ix, type) => stageMan.changeFx({stageId, ix, type})
   //+ ezt csak az uibol hivja a selektorfx ugye?
   
   //8#46f ------------- Sync control: Master/slave stage settings -------------
   
   const activateMaster = stageId => {
-    dis.senderStage = getStage(stageId)
-    dis.isMaster = true
-    dis.senderStage.fpo.panel.set('send', {class: 'active'}) 
+    pg.senderStage = getStage(stageId)
+    pg.isMaster = true
+    pg.senderStage.fpo.panel.set('send', {class: 'active'}) 
   }
   const activateSlave = stageId => {
-    dis.listenerStage = getStage(stageId)
-    dis.isSlave = true
-    dis.listenerStage.fpo.panel.set('listen', {class: 'active'}) 
+    pg.listenerStage = getStage(stageId)
+    pg.isSlave = true
+    pg.listenerStage.fpo.panel.set('listen', {class: 'active'}) 
   }
   const inactivateMaster = _ => {
-    void dis.senderStage?.fpo.panel.set('send', {declass: 'active'}) 
-    delete dis.senderStage
-    dis.isMaster = false
+    void pg.senderStage?.fpo.panel.set('send', {declass: 'active'}) 
+    delete pg.senderStage
+    pg.isMaster = false
   }
   const inactivateSlave = _ => {
-    void dis.listenerStage?.fpo.panel.set('listen', {declass: 'active'}) 
-    delete dis.listenerStage
-    dis.isSlave = false
+    void pg.listenerStage?.fpo.panel.set('listen', {declass: 'active'}) 
+    delete pg.listenerStage
+    pg.isSlave = false
   }
   const clearSendersListeners = _ => {
     inactivateMaster()
     inactivateSlave()
   }
-  dis.setSenderStage = (stageId = -1) => {
+  pg.setSenderStage = (stageId = -1) => {
     stageId === -1 && (stageId = 0) //: or getStageGroup's last Index
     const newSenderStage = getStage(stageId)
     clearSendersListeners()
     activateMaster(newSenderStage)
   }
-  dis.setListenerStage = stageId => {
+  pg.setListenerStage = stageId => {
     const newListenerStage = getStage(stageId)
     clearSendersListeners()
     activateSlave(newListenerStage)
@@ -132,7 +134,7 @@ const createPlayground = root => {
   const initStageSender = stageId => {//+ ez atlog a playersbe (amit at kene nevezni)
     const lazySend = startEndThrottle(stobj => {
       radio.postMessage(stobj)
-      dis.lastSentAt = NoW()
+      pg.lastSentAt = NoW()
       console.log('🏹sent', stobj)
     }, 50)
     
@@ -144,15 +146,15 @@ const createPlayground = root => {
         return //console.warn('no lastFixedFx', endRatio, endRatio.int.shared)
       }
       const isThisLastFixed = endRatio.int.shared.lastFixedFx === endRatio //+ was true
-      if (stage === dis.senderStage && endRatio.isActive && isThisLastFixed) {
-        lazySend({cmd: 'ratio', data: {gain: endRatio.getValue('gain')}, fp: dis.fingerPrint})
+      if (stage === pg.senderStage && endRatio.isActive && isThisLastFixed) {
+        lazySend({cmd: 'ratio', data: {gain: endRatio.getValue('gain')}, fp: pg.fingerPrint})
       }
     })
     // on bpm speed change
   }
   
-  dis.incomingRatio = gain => { //: called from pg-players
-    const endRatio = dis.listenerStage?.endRatio 
+  pg.incomingRatio = gain => { //: called from pg-players
+    const endRatio = pg.listenerStage?.endRatio 
     if (endRatio?.isActive) {
       console.log('🎯received', {gain, willgain: 1 - gain})
       endRatio.setValue('gain', 1 - gain)
@@ -161,7 +163,7 @@ const createPlayground = root => {
   
   //8#59c  ---------------- Stage init / change ----------------
   
-  dis.addStage = letter => {
+  pg.addStage = letter => {
     //const nuIx = ascii(letter) - ascii('A')
     const stage = stageMan.createStage({letter}, {hasEndSpectrum: true, hasEndRatio: true})
     const {stageIx} = stage
@@ -169,14 +171,14 @@ const createPlayground = root => {
     stage.assignUi({uiStage})
     stage.changeFx({ix: 0, type: 'fx_gain', params: {isFixed: true, hasStageMark: true}})
     stage.output.connect(output)
-    //dis.sources.changeStageSourceIndex(stageIx, 1, {isFirst: true}) //: 
+    //pg.sources.changeStageSourceIndex(stageIx, 1, {isFirst: true}) //: 
     //initStageSender(nuIx)
     return stageIx
   }
   
-  dis.rebuildStage = stageId => getStage(stageId)?.rebuild() //: re ui regen click
+  pg.rebuildStage = stageId => getStage(stageId)?.rebuild() //: re ui regen click
 
-  dis.activateStage = (stageId, on) => getStage(stageId)?.activate(on)
+  pg.activateStage = (stageId, on) => getStage(stageId)?.activate(on)
   
   //8#c78 --------- Entry point / init --------- 
   
@@ -187,51 +189,51 @@ const createPlayground = root => {
     players.initRadioListeners()
   }
   
-  dis.initMixerStages = _ => {
-    dis.localStage = stageMan.createStage({letter: dis.localStageLetter = 'LOC'})
-    dis.localStageIx = dis.localStage.stageIx
-    dis.remoteStage = stageMan.createStage({letter: dis.remoteStageLetter = 'REM'})
-    dis.remoteStageIx = dis.remoteStage.stageIx
-    dis.faderStage = stageMan.createStage({letter: dis.faderStageLetter = 'FAD'})
-    dis.faderStageIx = dis.faderStage.stageIx
+  pg.initMixerStages = _ => {
+    pg.localStage = stageMan.createStage({letter: pg.localStageLetter = 'LOC'})
+    pg.localStageIx = pg.localStage.stageIx
+    pg.remoteStage = stageMan.createStage({letter: pg.remoteStageLetter = 'REM'})
+    pg.remoteStageIx = pg.remoteStage.stageIx
+    pg.faderStage = stageMan.createStage({letter: pg.faderStageLetter = 'FAD'})
+    pg.faderStageIx = pg.faderStage.stageIx
     
     /* stage.assignUi({uiStage})
     stage.changeFx({type: 'fx_gain'})
     stage.output.connect(output)
-    dis.sources.changeStageSourceIndex(nuIx, 0, {isFirst: true}) // */
+    pg.sources.changeStageSourceIndex(nuIx, 0, {isFirst: true}) // */
   }
   
-  dis.changeDestination = newDestination => { //: not used
+  pg.changeDestination = newDestination => { //: not used
     output.disconnect()
     output.connect(newDestination)
   }
   
-  dis.insertBpmManipulator = _ => {
+  pg.insertBpmManipulator = _ => {
     /*
-    dis.bpmSource!!!
-    dis.source.disconnect()
-    dis.bpmTransformer.setValue('media', mediaElement)
+    pg.bpmSource!!!
+    pg.source.disconnect()
+    pg.bpmTransformer.setValue('media', mediaElement)
 
-    dis.bpmTransformer.setValue('bpmOriginal', dis.bpm)
+    pg.bpmTransformer.setValue('bpmOriginal', pg.bpm)
     ui.set('bpmMod', {declass: 'off'})
-    connectArr(dis.source, dis.bpmTransformer, input)
+    connectArr(pg.source, pg.bpmTransformer, input)
     */
   }
   
   //8#ca0 ---------BPM detect / adjust  --------- 
   /* 
-  dis.recalcBpm = async (calcSec = 15) => {
-    dis.bpmAuditor = dis.bpmAuditor || BPM.createBPMAuditor(waCtx)
-    dis.bpmAuditor.start(dis.source)
+  pg.recalcBpm = async (calcSec = 15) => {
+    pg.bpmAuditor = playground.bpmAuditor || BPM.createBPMAuditor(waCtx)
+    pg.bpmAuditor.start(playground.source)
     
     for (let elapsed = 0; elapsed < calcSec; elapsed++) { 
       ui.set('bpm', {text: `Listening for BPM (${calcSec - elapsed}s)...`})
       await adelay(1000)
     }
-    const {bpm, candidates, error} = await dis.bpmAuditor.stop()
+    const {bpm, candidates, error} = await playground.bpmAuditor.stop()
     if  (bpm > 55 && bpm < 200) {
-      dis.bpm = bpm
-      dis.insertBpmManipulator()
+      pg.bpm = bpm
+      pg.insertBpmManipulator()
       ui.set('bpmpp', {declass: 'off'})
     } else {
       ui.set('bpmpp', {class: 'off'})
@@ -239,27 +241,64 @@ const createPlayground = root => {
     ui.set('bpm', {text: `BPM:` + bpm})
   }
   
-  dis.bpmDelays = _ => {
+  pg.bpmDelays = _ => {
     for (const {fxArr} of stages) { //+ BAD! stage can be undefined
       for (const fx of fxArr) {
         if (fx.getName().includes('Pong')) {
-          fx.setValue('delayLeft', 240000 / dis.bpm)
-          fx.setValue('delayRight', 120000 / dis.bpm)
+          fx.setValue('delayLeft', 240000 / pg.bpm)
+          fx.setValue('delayRight', 120000 / pg.bpm)
         }
       }
     }
   } */
   
-  dis.addFx = (stageId, name) => stageMan.addFx(stageId, name)
+  pg.addFx = (stageId, name) => stageMan.addFx(stageId, name)
   
+  pg.setPreset = (setup, setupName) => {
+    const stageNames = []
+    
+    for (const key in setup) {
+      stageNames.includes(key) || stageNames.push(key)
+    }
+    stageNames.sort()
+    console.log({stageNames})
+    for (const stageName of stageNames) {
+      pg.addStage(stageName)
+    }      
+    for (const key in setup) {
+      const arr = setup[key]
+      for (const fx of arr) {
+        fx && pg.addFx(key, fx[2] === '_' ? fx : ('fx_' + fx)) 
+      }
+    }
+    ui.finalize()
+    
+    if (setupName === 'scopeChain') { //: special case -> save full stage instead
+      const DELAY = .368
+      for (let i = 0; i < 12; i++) {
+        const fx = pg.stages[i].fxArr[1]
+        if (fx) {
+          fx.setValue('delayTime', (11 - i) * DELAY)
+        }
+        const gx = pg.stages[i].fxArr[2]
+        if (gx) {
+          gx.setValue('fullZoom', 'fire')
+        }
+        console.log(fx.zholger)
+      }
+      void pg.stages[3]?.stEndRatio.setValue('gain', 1)
+    }
+  }
+
   init()
   
-  return dis
+  return pg
 }
 
-export const runPlaygroundWithin = (waCtx, options) => { //: no mediaElement, ABSNs will be added
+//8#b39 All entry points are here (beeFxPlayground site, CromBee, Patashnik)
+
+export const runPlaygroundWithin = async (waCtx, options) => { //: no mediaE, ABSNs will be added
   const config = {
-    sourceListDisplayOn: true,
     maxSources: 8,
     ...options
   }
@@ -270,65 +309,29 @@ export const runPlaygroundWithin = (waCtx, options) => { //: no mediaElement, AB
     ...options
   }
   root.ui = createUI(root)
-  const playground = createPlayground(root)
+  const playground = await createPlayground(root)
+  root.ui.start(playground) //+ csekk disz
   return {root, playground}
 }
 
 export const runPlayground = async root => { //: we may have a mediaElement in root
-  const {ui, waCtx} = root
-  const playground = createPlayground(root)
+  const ui = root.ui = createUI(root)
+  root.midi = pgIm.TestMidi?.createTestMidi(ui) //: MIDI test, can be deleted
+  const playground = await createPlayground(root)
   ui.start(playground)
-  
-  root.midi = pgIm.TestMidi?.createTestMidi(ui)
-
-  if (root.onYoutube) {
-    root.mediaElement && ui.changeVideoElementSource(1, root.mediaElement)
-  } else {
-    playground.sources.getValidSourcesCnt() || ui.toggleList() //: only if there are no sources
-  }
 
   const setupName = root.onYoutube 
-    ? root.killEmAll
-      ? 'youtubeFull'
-      : 'youtubeMinimal'
-    : 'last' // 'scopeChain' //Golem // setupPresetDebug
+    ? root.killEmAll ? 'youtubeFull' : 'youtubeDefault'
+    : 'last'
   
   const parent$ = root.config.presetDisplayOn ? document.body : undef
   
   StateManager.getActualPreset({name: setupName, parent$})
     .then(setup => {
-      const stageNames = []
+      playground.setPreset(setup, setupName)
       
-      for (const key in setup) {
-        stageNames.includes(key) || stageNames.push(key)
-      }
-      stageNames.sort()
-      console.log({stageNames})
-      for (const stageName of stageNames) {
-        playground.addStage(stageName)
-      }      
-      for (const key in setup) {
-        const arr = setup[key]
-        for (const fx of arr) {
-          fx && playground.addFx(key, fx[2] === '_' ? fx : ('fx_' + fx)) 
-        }
-      }
-      ui.finalize()
-      
-      if (setupName === 'scopeChain') { //: special case -> save full stage instead
-        const DELAY = .368
-        for (let i = 0; i < 12; i++) {
-          const fx = playground.stages[i].fxArr[1]
-          if (fx) {
-            fx.setValue('delayTime', (11 - i) * DELAY)
-          }
-          const gx = playground.stages[i].fxArr[2]
-          if (gx) {
-            gx.setValue('fullZoom', 'fire')
-          }
-          console.log(fx.zholger)
-        }
-        void playground.stages[3]?.stEndRatio.setValue('gain', 1)
-      }
+      root.onYoutube
+        ? root.mediaElement && ui.changeVideoElementSource(1, root.mediaElement)
+        : playground.sources.getValidSourcesCnt() || ui.toggleList() //: only if no sources
     })
 }
