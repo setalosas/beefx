82dea9e577b82f8de04879ca50262468475bd049
setalosas <setalosas@gmail.com>
2021-04-17 01:01:16 +0200

    Players ui added.

--- js/improxy-esm.js
+++ js/improxy-esm.js
@@ -10,21 +10,24 @@ import { BeeFX, BPM, onWaapiReady, Visualizer, createGraphBase } from './beefx/b
 import * as Midi from './red/esm/webapis/midi-interface-esm.js'
 import * as TestMidi from './ui/ui-midi-esm.js'
 
-import * as FxUi from './ui/ui-fxpanel-esm.js'
-import * as StagesUi from './ui/ui-stages-esm.js'
-import * as SourcesUi from './ui/ui-sources-esm.js'
-import * as MixerUi from './ui/ui-mixer-esm.js'
-import { createUI } from './ui/ui-esm.js'
-
 import * as Sources from './pg-sources-esm.js'
 import * as Players from './pg-players-esm.js'
 import * as StateManager from './pg-states-esm.js'
 import * as StageManager from './pg-stages-esm.js'
+
+import * as FxUi from './ui/ui-fxpanel-esm.js'
+import * as StagesUi from './ui/ui-stages-esm.js'
+import * as PlayersUi from './ui/ui-players-esm.js'
+import * as SourcesUi from './ui/ui-sources-esm.js'
+import * as MixerUi from './ui/ui-mixer-esm.js'
+
+import { createUI } from './ui/ui-esm.js'
+
 import * as Playground from './playground-esm.js'
 
 export {
   Corelib, Store, DOMplusUltra, Midi, TestMidi,
   onWaapiReady, BeeFX, BPM, Visualizer, createGraphBase,
   Sources, Players, StateManager, StageManager, Playground,
-  StagesUi, FxUi, SourcesUi, MixerUi, createUI
+  StagesUi, FxUi, PlayersUi, SourcesUi, MixerUi, createUI
 }
