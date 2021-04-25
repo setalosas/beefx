bdbe72dd96fd07ac5db5c5767289c375067455ec
setalosas <setalosas@gmail.com>
2021-04-25 07:00:18 +0200

    Players & MixerUi gone.

--- js/improxy-esm.js
+++ js/improxy-esm.js
@@ -11,15 +11,14 @@ import * as Midi from './red/esm/webapis/midi-interface-esm.js'
 import * as TestMidi from './ui/ui-midi-esm.js'
 
 import * as Sources from './pg-sources-esm.js'
-import * as Players from './pg-players-esm.js'
 import * as StateManager from './pg-states-esm.js'
 import * as StageManager from './pg-stages-esm.js'
 
+import * as FxUiPars from './ui/ui-fxpars-esm.js'
 import * as FxUi from './ui/ui-fxpanel-esm.js'
 import * as StagesUi from './ui/ui-stages-esm.js'
 import * as PlayersUi from './ui/ui-players-esm.js'
 import * as SourcesUi from './ui/ui-sources-esm.js'
-import * as MixerUi from './ui/ui-mixer-esm.js'
 import * as StatesUi from './ui/ui-states-esm.js'
 
 import { createUI } from './ui/ui-esm.js'
@@ -29,6 +28,6 @@ import * as Playground from './playground-esm.js'
 export {
   Corelib, Store, DOMplusUltra, DragWithDOM, Midi, TestMidi,
   onWaapiReady, BeeFX, BPM, Visualizer, createGraphBase,
-  Sources, Players, StateManager, StageManager, Playground,
-  StagesUi, FxUi, PlayersUi, SourcesUi, MixerUi, StatesUi, createUI
+  Sources, StateManager, StageManager, Playground,
+  StagesUi, FxUiPars, FxUi, PlayersUi, SourcesUi, StatesUi, createUI
 }
