edaea4a47489bb7550512e3ef37921b172bc3a7d
setalosas <setalosas@gmail.com>
2021-04-29 03:17:57 +0200

    MASNI, cleanup.

--- js/beefx/fxs/beefxs-osc-esm.js
+++ js/beefx/fxs/beefxs-osc-esm.js
@@ -2,19 +2,16 @@
    object-curly-spacing, no-trailing-spaces, indent, new-cap, block-spacing, comma-spacing,
    handle-callback-err, no-return-assign, camelcase, yoda, object-property-newline,
    no-void, quotes, no-floating-decimal, import/first, space-unary-ops, 
-   no-unused-vars, standard/no-callback-literal, object-curly-newline */
+   standard/no-callback-literal, object-curly-newline */
    
-import {Corelib, BeeFX, onWaapiReady} from '../beeproxy-esm.js'
-
-const {nop, isArr, getRnd, getRndFloat} = Corelib
-const {wassert} = Corelib.Debug
-const {round, PI} = Math
-const PI2 = 2 * PI
+import {BeeFX, onWaapiReady} from '../beeproxy-esm.js'
 
 onWaapiReady.then(waCtx => {
   const {registerFxType, newFx, connectArr} = BeeFX(waCtx)
+  
+  //: Mostly half-baked experimental stuff. WIP.
 
-  const moogFx = { //8#c6c ------- moog (Chris Wilson) -------
+  const moogFx = { //8#c6c ------- moog (Tuna / Chris Wilson) -------
     def: {
       cutoff: {defVal: .065, min: 0.001, max: 1},
       resonance: {defVal: 3.99, min: 0, max: 4}
@@ -28,7 +25,7 @@ onWaapiReady.then(waCtx => {
   }[key])
   
   moogFx.construct = (fx, {initial}, {int} = fx) => {
-    const bufferSize = 8192 // 4096 // 16384
+    const bufferSize = 1024 // 4096 //  // 4096 // 16384
     int.moog = waCtx.createScriptProcessor(bufferSize, 1, 1)
     let in1, in2, in3, in4, out1, out2, out3, out4
     in1 = in2 = in3 = in4 = out1 = out2 = out3 = out4 = 0.0
@@ -59,54 +56,59 @@ onWaapiReady.then(waCtx => {
   
   registerFxType('fx_moog', moogFx)
 
-  const vibratoFx = { //8#6b6 ------- vibrato (Chris Wilson?) -------
+  const vibratoFx = { //8#6b6 ------- vibrato (Chris Wilson live audio effects) -------
     def: {
-      speed: {defVal: 3.5, min: .5, max: 15},
-      delay: {defVal: 30, min: 5, max: 55},
-      depth: {defVal: 2, min: .5, max: 4}
+      speed: {defVal: 3.5, min: .5, max: 15, unit: 'Hz', prec: 3},
+      delay: {defVal: 30, min: 5, max: 55, unit: 'ms'},
+      depth: {defVal: 2, min: .5, max: 20, subType: 'exp'}
     },
     midi: {pars: ['speed,delay,depth']}
   }
-  vibratoFx.construct = (fx, {initial}) => {
-    const {int} = fx
-    
-    int.delayNode = waCtx.createDelay(1)
-    int.delayNode.delayTime.value = initial.delay / 1000
 
-    int.osc = waCtx.createOscillator()
-    int.gain = waCtx.createGain()
-
-    int.gain.gain.value = initial.depth / 1000
-    int.osc.type = 'sine'
-    int.osc.frequency.value = initial.speed
-    int.osc.connect(int.gain)
-    int.gain.connect(int.delayNode.delayTime)
-    
-    fx.start.connect(int.delayNode)
-    int.delayNode.connect(fx.output)
-    int.osc.start(0)
-  }
   vibratoFx.setValue = (fx, key, value) => ({
     speed: _ => fx.setAt('osc', 'frequency', value),
     delay: _ => fx.setDelayTime('delayNode', value / 1000),
     depth: _ => fx.setAt('gain', 'gain', value / 1000)
   }[key])
   
+  vibratoFx.construct = (fx, pars, {int} = fx) => {
+    int.delayNode = waCtx.createDelay(1)
+    int.osc = waCtx.createOscillator()
+    int.gain = waCtx.createGain()
+    int.osc.type = 'sine'
+    int.osc.connect(int.gain)
+    int.gain.connect(int.delayNode.delayTime)
+    fx.start.connect(int.delayNode)
+    int.delayNode.connect(fx.output)
+    int.osc.start(0)
+  }
+
   registerFxType('fx_vibrato', vibratoFx)
   
   //+ This is quite bad ATM, needs fxzing.
+  
+  const biquadOptions = [ //: DRY this, it's also in basic / biquadFilter
+    ['lowpass', 'lowpass [no gain]'],
+    ['highpass', 'highpass [no gain]'],
+    ['bandpass', 'bandpass [no gain]'],
+    ['lowshelf', 'lowshelf, [no Q]'],
+    ['highshelf', 'highshelf [no Q]'],
+    ['allpass', 'allpass [no gain]'],
+    ['notch', 'notch [no gain]'],
+    ['peaking', 'peaking']
+  ]
 
   const autoWahFx = { //8#04c ------- autoWah (Chris Wilson) -------
     def: {
-      followerFilterType: {defVal: 'lowpass', type: 'string', subType: 'biquad'},
-      followerFrequency: {defVal: 10, min: 5, max: 200, unit: 'Hz'},
+      followerFilter: {defVal: 'lowpass', type: 'strings', subType: biquadOptions},
+      followerFreq: {defVal: 10, min: 5, max: 200, unit: 'Hz'},
       depth: {defVal: 11585, min: 500, max: 20000},
       filterType: {defVal: 'lowpass', type: 'string', subType: 'biquad'},
       frequency: {defVal: 50, min: 10, max: 200, unit: 'Hz'},
       Q: {defVal: 15, min: 0, max: 30},
       freqGraph: {type: 'graph'}
     },
-    midi: {pars: ['followerFrequency,depth', 'frequency,Q']},
+    midi: {pars: ['followerFreq,depth', 'frequency,Q']},
     graphs: {
       freqGraph: [{
         graphType: 'freq',
@@ -128,31 +130,25 @@ onWaapiReady.then(waCtx => {
   }
 
   autoWahFx.setValue = (fx, key, value, {int} = fx) => ({
-    followerFilterType: _ => int.awFollower.type = value,
-    followerFrequency: _ => fx.setAt('awFollower', 'frequency', value),
+    followerFilter: _ => int.awFollower.type = value,
+    followerFreq: _ => fx.setAt('awFollower', 'frequency', value),
     depth: _ => fx.setAt('awDepth', 'gain', value),
     filterType: _ => int.awFilter.type = value,
     Q: _ => fx.setAt('awFilter', 'Q', value),
     frequency: _ => fx.setAt('awFilter', 'frequency', value)
   }[key])
 
-  autoWahFx.construct = (fx, {initial}, {int} = fx) => {
+  autoWahFx.construct = (fx, pars, {int} = fx) => {
     int.waveshaper = waCtx.createWaveShaper()
     int.awFollower = waCtx.createBiquadFilter()
-    int.awFollower.type = initial.followerFilterType
-    int.awFollower.frequency.value = initial.followerFrequency
 
     const curve = new Float32Array(65536)
     for (let i = -32768; i < 32768; i++) {
-      curve[i + 32768] = ((i > 0) ? i : -i) / 32768
+      curve[i + 32768] = Math.abs(i) / 32768
     }
     int.waveshaper.curve = curve
     int.awDepth = waCtx.createGain()
-    int.awDepth.gain.value = initial.depth
     int.awFilter = waCtx.createBiquadFilter()
-    int.awFilter.type = initial.filterType
-    int.awFilter.Q.value =  initial.Q
-    int.awFilter.frequency.value = initial.frequency
     connectArr(fx.start, int.waveshaper, int.awFollower, int.awDepth, int.awFilter.frequency)
     connectArr(fx.start, int.awFilter, fx.output)
   }
@@ -166,10 +162,8 @@ onWaapiReady.then(waCtx => {
     const {int} = fx
     
     int.pingPong = newFx('fx_pingPongDelayA')
-    int.pingPong.connect(fx.output) //: a pingpongdelay a legutolso
-    
-    //int.pingPong.input.connect(fx.start)//+was wetgain??????????? inkabb fx,.output az
-    int.pingPong.input.connect(fx.output)
+    int.pingPong.connect(fx.output)
+    //int.pingPong.input.connect(fx.output)
     
     int.autoWah = newFx('fx_autoWah')
     int.autoWah.connect(int.pingPong)
