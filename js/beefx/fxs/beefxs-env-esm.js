3106570424fa5ed4e72fc27eed2631865e3f3954
setalosas <setalosas@gmail.com>
2021-04-29 03:13:40 +0200

    MASNI, activation instead of state hack.

--- js/beefx/fxs/beefxs-env-esm.js
+++ js/beefx/fxs/beefxs-env-esm.js
@@ -1,24 +1,28 @@
-/* eslint-disable no-debugger, spaced-comment, no-multi-spaces, valid-typeof, 
+/* eslint-disable no-debugger, spaced-comment, no-multi-spaces, valid-typeof, import/first,
    object-curly-spacing, no-trailing-spaces, indent, new-cap, block-spacing, comma-spacing,
    handle-callback-err, no-return-assign, camelcase, yoda, object-property-newline,
-   no-void, quotes, no-floating-decimal, import/first, space-unary-ops, 
+   no-void, quotes, no-floating-decimal, space-unary-ops, 
    standard/no-callback-literal, object-curly-newline */
    
 import {Corelib, BeeFX, onWaapiReady} from '../beeproxy-esm.js'
 
 const {nop, clamp} = Corelib
-const {weject} = Corelib.Debug
+const {wejectNaN} = Corelib.Debug
+const {startEndThrottle} = Corelib.Tardis
+const {min, pow, exp, sqrt, round} = Math
 
 onWaapiReady.then(waCtx => {
   const {connectArr, registerFxType, newFx} = BeeFX(waCtx)
+  const {sampleRate} = waCtx
+  const nyquist = sampleRate / 2
+  const logOn = false
 
-  const envelopeFollowerFx = { //8#04c ------- envelopeFollower (Tuna) -------
+  const envelopeFollowerFx = { //8#04c ------- envelopeFollower (adapted from Tuna) -------
     def: {
-      attackTime: {defVal: 3, min: 0, max: 5000}, // /1000
-      releaseTime: {defVal: 500, min: 0, max: 500}, // /1000
+      attackTime: {defVal: 3, min: 0, max: 5000}, // sec * 1000
+      releaseTime: {defVal: 500, min: 0, max: 500}, // sec * 1000
       masterFx: {defVal: {}},
-      target: {defVal: {}, subType: 'skipui'},
-      callback: {defVal: nop, subType: 'skipui'}
+      callback: {defVal: nop, skipUi: true}
     },
     uiSelectDisabled: true
   }
@@ -26,99 +30,115 @@ onWaapiReady.then(waCtx => {
   envelopeFollowerFx.setValue = ({int}, key, value) => ({
     attackTime: _ => {
       int._attackTime = value / 1000
-      int._attackC = Math.exp(-1 / int._attackTime * int.sampleRate / int.buffersize)
-      weject(Number.isNaN(int._attackC))
+      int._attackC = exp(-1 / int._attackTime * sampleRate / int.buffersize)
+      wejectNaN(int._attackC)
     },
     releaseTime: _ => {
       int._releaseTime = value / 1000
-      int._releaseC = Math.exp(-1 / int._releaseTime * int.sampleRate / int.buffersize)
-      weject(Number.isNaN(int._releaseC))
+      int._releaseC = exp(-1 / int._releaseTime * sampleRate / int.buffersize)
+      wejectNaN(int._releaseC)
     },
-    masterFx: _ => int.masterState = value.int.state,
-    target: _ => int._target = value,
-    callback: _ => int._callback = value // int.lfo.onaudioprocess = int.getCallbackPass(value)
+    masterFx: nop,
+    callback: nop
   }[key])
   
-  envelopeFollowerFx.construct = (fx, {initial}) => {
-    const {int} = fx
-    
+  envelopeFollowerFx.construct = (fx, pars, {int, atm} = fx) => {
     int.buffersize = 256    //: 256 is .005s, 512 is .012s, 16384 is .37s
     int.envelope = 0
-    int._envelope = 0
-    int.sampleRate = 44100
-    
-    int.jsNode = fx.output = waCtx.createScriptProcessor(int.buffersize, 1, 1)
+    int.scriptNode = fx.output = waCtx.createScriptProcessor(int.buffersize, 1, 1)
     fx.input.connect(fx.output)
 
-    /* int.getCallbackPass = callback => _ => {
-      wassert(int.masterState.thisIsState)
-      if (int.masterState.on) {
-        int._phase += int._phaseInc
-        int._phase %= PI2
-        callback(int._target, int._offset + int._oscillation * Math.sin(int._phase))
-      }
-    } */
-    
-    int.compute = event => {
-      const count = event.inputBuffer.getChannelData(0).length
-      const channels = event.inputBuffer.numberOfChannels
-      
-      let current
-      let chan = 0
-      let rms = 0
-      
-      if (channels > 1) { //need to mixdown
-        for (let i = 0; i < count; ++i) {
-          for (; chan < channels; ++chan) {
-            current = event.inputBuffer.getChannelData(chan)[i]
-            rms += (current * current) / channels
+    int.compute = ({inputBuffer}) => {
+      if (atm.masterFx.isActive) {
+        const count = inputBuffer.getChannelData(0).length
+        const channels = inputBuffer.numberOfChannels
+        let rms = 0
+        
+        for (let chan = 0; chan < channels; ++chan) { //: always 1 if we give 1, 1 as params above
+          const data = inputBuffer.getChannelData(chan)
+          for (let i = 0; i < count; ++i) {
+            const current = data[i]
+            rms += current * current
           }
         }
-      } else {
-        for (let i = 0; i < count; ++i) {
-          current = event.inputBuffer.getChannelData(0)[i]
-          rms += (current * current)
-        }
-      }
-      rms = Math.sqrt(rms)
+        rms /= channels
+        rms = sqrt(rms)
 
-      if (int._envelope < rms) {
-        int._envelope *= int._attackC
-        int._envelope += (1 - int._attackC) * rms
-      } else {
-        int._envelope *= int._releaseC
-        int._envelope += (1 - int._releaseC) * rms
+        if (int.envelope < rms) {
+          int.envelope *= int._attackC
+          int.envelope += (1 - int._attackC) * rms
+        } else {
+          int.envelope *= int._releaseC
+          int.envelope += (1 - int._releaseC) * rms
+        }
+        atm.callback(int.envelope)
       }
-      int._callback(int._envelope)
     }
   }
   envelopeFollowerFx.activate = ({int}, on) => {
     if (on) {
-      int.jsNode.connect(waCtx.destination)
-      int.jsNode.onaudioprocess = int.compute // int.returnCompute(this)
+      int.scriptNode.connect(waCtx.destination)
+      int.scriptNode.onaudioprocess = int.compute
     } else {
-      int.jsNode.disconnect()
-      int.jsNode.onaudioprocess = null
+      int.scriptNode.disconnect()
+      int.scriptNode.onaudioprocess = null
     }
   }
   registerFxType('fx_envelopeFollower', envelopeFollowerFx)
   
   const wahWahEFFx = { //8#a6e ---------- wahWahEF (Tuna) ----------
     def: {
+      baseFreq: {defVal: 50, min: 20, max: 10000, unit: 'Hz', subType: 'exp'},
+      excursionOctaves: {defVal: 2, min: 1, max: 6, name: 'excursionOct'},
+      resonance: {defVal: 10, min: 1, max: 100, subType: 'exp'},
       automode: {defVal: true, type: 'boolean'},
-      baseFrequency: {defVal: .5, min: 0, max: 1},
-      excursionOctaves: {defVal: 2, min: 1, max: 6},
-      sweep: {defVal: .2, min: 0, max: 10},
-      resonance: {defVal: 10, min: 1, max: 100},
-      sensitivity: {defVal: .5, min: -1, max: 1}
+      envelope: {defVal: .2, min: 0, max: 1, readOnly: true, name: 'envFollower'},
+      sweep: {defVal: .2, min: 0, max: 1, name: 'sweep'},
+      sensitivity: {defVal: 3, min: .1, max: 10, subType: 'exp'},
+      filterGraph: {type: 'graph'}
     },
-    midi: {pars: ['baseFrequency,excursionOctaves', 'sweep,resonance,sensitivity']},
-    name: 'Wah-Wah (EF)'
+    midi: {pars: ['baseFreq,excursionOctaves,resonance', 'sensitivity', 'sweep']},
+    name: 'Wah-Wah (EF)',
+    graphs: {
+      filterGraph: [{
+        graphType: 'freq',
+        triggerKeys: ['filterGraph'],
+        filter: 'filterBp',
+        minDb: -27,
+        maxDb: 33,
+        diynamic: .8,
+        customRenderer: {
+          pre: ({fx, cc, ccext, freq}) => {
+            const baseX = freq.freq2X[round(fx.atm.baseFreq)] || 0
+            const excurX = freq.freq2X[round(fx.int._excursionFreq)] || 0
+            const gradient = cc.createLinearGradient(baseX, 0, excurX, 0)
+            gradient.addColorStop(0, `hsla(120,75%,50%,.5)`)
+            gradient.addColorStop(.5, `hsla(60,75%,50%,.5)`)
+            gradient.addColorStop(1, `hsla(0,75%,50%,.5)`)
+            cc.fillStyle = gradient
+            cc.fillRect(baseX, 0, excurX, ccext.height)
+          }
+        },
+        phaseCurveColor: `hsla(330, 99%, 75%, .5)`,
+        magCurveColor: `hsl(330, 99%, 75%)`
+      }, {
+        graphType: 'freq',
+        triggerKeys: ['filterGraph'],
+        filter: 'filterPeaking',
+        renderSet: {doClear: false, doGrid: false, doGraph: true},
+        minDb: -27,
+        maxDb: 33,
+        diynamic: .8,
+        phaseCurveColor: `hsla(70, 99%, 75%, .5)`,
+        magCurveColor: `hsl(70, 99%, 75%)`
+      }]
+    }
   }
   
-  wahWahEFFx.setValue = (fx, key, value, {int} = fx) => ({
+  wahWahEFFx.onActivated = (fx, isActive) => fx.int.envelopeFollower.activate(isActive)
+  
+  wahWahEFFx.setValue = (fx, key, value, {int, atm} = fx) => ({
     automode: _ => {
-      int._automode = value
       if (value) {
         fx.start.connect(int.envelopeFollower.input)
         int.envelopeFollower.activate(true)
@@ -128,82 +148,80 @@ onWaapiReady.then(waCtx => {
         fx.start.connect(int.filterBp)
       }
     },
-    baseFrequency: _ => {
-      int._baseFreq = 50 * Math.pow(10, value * 2)
-      int._excursionFreq = Math.min(int.sampleRate / 2, int._baseFreq * Math.pow(2, int._excursionOctaves))
-      weject(Number.isNaN(int._excursionFreq))
-      int.setFilterFreq()
-    },
-    excursionOctaves: _ => {
-      int._excursionOctaves = value
-      int._excursionFreq = Math.min(int.sampleRate / 2, int._baseFreq * Math.pow(2, int._excursionOctaves))
-      weject(Number.isNaN(int._excursionFreq))
-      int.setFilterFreq()
-    },
-    sweep: _ => {
-      int._sweep = Math.pow(clamp(value, 0, 1), int._sensitivity)
-      //window.envdebug.push({at: window.performance.now(), msg: 'sweep: ' + int._sweep})
-      int.setFilterFreq()
-    },
+    baseFreq: _ => int.freqParsChanged(),
+    excursionOctaves: _ => int.freqParsChanged(),
+    sweep: _ => fx.setSweep(value, true),
+    envelope: nop,
     resonance: _ => {
-      int.filterPeaking.Q.value = int._resonance = value
+      int.filterPeaking.Q.value = value
+      int.freqParsChanged()
     },
-    sensitivity: _ => {
-      int._sensitivity = Math.pow(10, value)
-    }
+    sensitivity: _ => int._sensitivity = pow(10, value)
   }[key])
-
-  wahWahEFFx.onActivated = (fx, on) => fx.int.state.on = on
   
-  wahWahEFFx.construct = (fx, {initial}) => {
-    const {int} = fx
-    
-    int.state = {thisIsState: true}
+  window.envdebug = []
+  window.envcnt = 0
+
+  wahWahEFFx.construct = (fx, pars, {int, atm} = fx) => {
     int.filterFreqTimeout = 0
-    int.sampleRate = 44100
-    int._excursionOctaves = initial.excursionOctaves
-    int._baseFreq = initial.baseFrequency
-    int._sensitivity = initial.sensitivity
-    int._sweep = initial.sweep
+    
+    const lazyLog = startEndThrottle(console.log, 200)
+    
+    const redraw = startEndThrottle(_ => fx.valueChanged('filterGraph'), 20)
     
     int.setFilterFreq = _ => {
       let freq
       try {
-        freq = Math.min(22050, int._baseFreq + int._excursionFreq * int._sweep)
-        weject(Number.isNaN(freq))
+        freq = min(nyquist, atm.baseFreq + int._excursionFreq * atm.sweep)
+        
+        if (logOn) {
+          wejectNaN(freq)
+          const {baseFreq, sweep, sensitivity} = atm
+          const {_excursionFreq} = int
+          lazyLog({freq, baseFreq, _excursionFreq, sweep, sensitivity})
+        }
+        
         int.filterBp.frequency.value = freq
         int.filterPeaking.frequency.value = freq
       } catch (err) {
-        console.err('setfiltfreq', err)
+        console.error('setfiltfreq', err)
         clearTimeout(int.filterFreqTimeout)
         //put on the next cycle to let all init properties be set
         int.filterFreqTimeout = setTimeout(int.setFilterFeq, 0)
       }
-      //window.envdebug.push({at: window.performance.now(), msg: 'freq: ' + freq})
+      redraw()
+    }
+    
+    int.freqParsChanged = _ => {
+      int._excursionFreq = min(nyquist, atm.baseFreq * pow(2, atm.excursionOctaves))
+      int.setFilterFreq()
+    }
+    
+    fx.setSweep = (sweep, isManual = false) => {
+      if (!isManual) { //: sweep is env as it comes from the envelopeFollower
+        fx.setValue('envelope', sweep)
+        fx.setValue('sweep', pow(clamp(sweep, 0, 1), atm.sensitivity))
+      }
+      int.setFilterFreq()
     }
     
     int.envelopeFollower = newFx('fx_envelopeFollower', {initial: {
       masterFx: fx,
-      target: int,
-      callback: (value) => fx.setValueIf('sweep', value)
+      callback: fx.setSweep
     }})
     
     int.filterBp = waCtx.createBiquadFilter()
     int.filterBp.type = 'bandpass'
-    int.filterBp.frequency.value = 100
     int.filterBp.Q.value = 1
     
     int.filterPeaking = waCtx.createBiquadFilter()
     int.filterPeaking.type = 'peaking'
-    int.filterPeaking.frequency.value = 100
     int.filterPeaking.gain.value = 20
-    int.filterPeaking.Q.value = 5
     
     connectArr(fx.start, int.filterBp, int.filterPeaking, fx.output)
 
     fx.output.gain.value = 1
     fx.start.gain.value = 2
-    int.envelopeFollower.activate(true)
   }
   registerFxType('fx_wahWahEF', wahWahEFFx)
 })
