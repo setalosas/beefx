1e15d52c2e2b582a638350e2077e61f62e0bb1b2
setalosas <setalosas@gmail.com>
2021-05-15 06:19:48 +0200

    +state$ (DOM state cache)

--- js/red/esm/dom/dom-plus-ultra-esm.js
+++ js/red/esm/dom/dom-plus-ultra-esm.js
@@ -77,10 +77,11 @@ export const css$ = (node, css) => {
 }
 export const set$ = (node, pars = {}, children) => {
   pars.class = pars.class || pars.cclass
-  const {css, attr, deattr, text, html, class: cclass = [], declass = [], on = {}, click} = pars
+  const {css, attr = {}, id, text, html, value, class: cclass = [], on = {}, click} = pars
+  const {deattr, declass = []} = pars
+  id && (attr.id = id)
   const classList = isArr(cclass) ? cclass : cclass ? cclass.split(' ').filter(a => a) : []
   const declassList = isArr(declass) ? declass : declass ? declass.split(' ').filter(a => a) : []
-  
   classList.length && node.classList.add(...classList)
   declassList.length && node.classList.remove(...declassList)
   for (const key in attr) {
@@ -90,21 +91,25 @@ export const set$ = (node, pars = {}, children) => {
     node.removeAttribute(key)
   }
   css$(node, css)
+  typeof children === 'string' && (node.textContent = children)
   if (typeof text === 'string') {
     node.textContent = text 
   } else if (typeof text === 'number') {
     node.textContent = text + ''
   }
   isStr(html) && (node.innerHTML = html)
+  typeof value !== Ø && (node.value = value)
   for (const eventType in on) {
     node.addEventListener(eventType, on[eventType])
   }
   click && node.addEventListener('click', click)//:shortcut
-  if (children) {
+  if (children && typeof children === 'object') {
     children.nodeType && (children = [children])
     wassert(isArr(children))
     for (const child of children) {
-      child && child.nodeType && node.appendChild(child)
+      typeof child === 'string'
+        ? node.appendChild(document.createTextNode(child))
+        : child && child.nodeType && node.appendChild(child)
     }
   }
   return node
@@ -123,6 +128,89 @@ export const span$ = (attr, children) => leaf$('span', attr, children)
 export const frag$ = children => leaf$('frag', {}, children)
 
 export const end$ = (type, root, pars, children) => root.appendChild(leaf$(type, pars, children))
+
+export const D = {c: leaf$}
+D.extend = sa => sa.split(',').map(tag => D[tag] = (...args) => leaf$(tag, ...args))
+D.extend('div,span,a,h1,h2,h3,strong,em,form,label,input,button,ul,li')
+
+export const state$ = node => {
+  wassert(node)
+  
+  const state = {
+    classHash: {},
+    attr: {},
+    css: {},
+    text: node.textContent,
+    html: node.innerHTML
+  }
+  const reHash = _ => {
+    state.classHash = {}
+    for (const cl of [...node.classList]) {
+      state.classHash[cl] = true
+    }
+    //:css not important - will be filled on first sets, the perf only important later
+  }
+  reHash()
+  
+  state.set = pars => {
+    pars.class = pars.class || pars.cclass
+    const {css, attr = {}, text, html, value, class: cclass = []} = pars
+    const {deattr = {}, declass = []} = pars
+    const classList = isArr(cclass) ? cclass : cclass ? cclass.split(' ').filter(a => a) : []
+    const declassList = isArr(declass) ? declass : declass ? declass.split(' ').filter(a => a) : []
+    let doSet = false
+    for (const cl of classList) {
+      if (!state.classHash[cl]) {
+        state.classHash[cl] = true
+        doSet = true
+      }
+    }
+    for (const cl of declassList) {
+      if (state.classHash[cl]) {
+        delete state.classHash[cl]
+        doSet = true
+      }
+    }
+    for (const prop in css) {
+      if (state.css[prop] !== css[prop]) {
+        state.css[prop] = css[prop]
+        doSet = true
+      }
+    }
+    for (const key in attr) {
+      if (state.attr[key] !== attr[key]) {
+        state.attr[key] = attr[key]
+        doSet = true
+      }
+    }
+    for (const key in deattr) {
+      if (state.attr[key]) {
+        delete state.attr[key]
+        doSet = true
+      }
+    }
+    if (isStr(text) && state.text !== text) {
+      state.text = text
+      doSet = true
+    }
+    if (isStr(html) && state.html !== html) {
+      state.html = html
+      doSet = true
+    }
+    if (typeof value !== Ø) {
+      state.value = value
+      doSet = true
+    }
+    doSet && set$(node, pars)
+    //console.log({doSet, node}, pars, state)
+    return node //????
+  }
+  state.get = _ => state
+  
+  state.remove = _ => node.remove() //: no need for this
+  
+  return state
+}
 
 export const haltEvent = event => {
   event.preventDefault()  
