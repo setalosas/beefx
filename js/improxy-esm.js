ba0bce4aebb31099a654b82a5c8eacb7c81215c0
setalosas <setalosas@gmail.com>
2021-05-15 06:19:06 +0200

    Restructuring (Observer & React, standard libs.)_

--- js/improxy-esm.js
+++ js/improxy-esm.js
@@ -4,8 +4,12 @@ import * as Corelib from './red/esm/stdlib/corelib-esm.js'
 import * as Store from './red/esm/stdlib/store-esm.js'
 import * as DOMplusUltra from './red/esm/dom/dom-plus-ultra-esm.js'
 import { DragWithDOM } from './red/esm/dom/dragwdom-esm.js'
+import { R } from './red/esm/dom/redact-esm.js'
 
-import { BeeFX, BPM, onWaapiReady, Visualizer, createGraphBase } from './beefx/beeproxy-esm.js'
+import { BeeFX, BPM, onWaapiReady } from './beefx/beeproxy-esm.js'
+
+import { createGraphBase } from './vis/graphbase-esm.js'
+import * as Visualizer from './vis/visualizer-esm.js'
 
 import * as Midi from './red/esm/webapis/midi-interface-esm.js'
 import * as TestMidi from './ui/ui-midi-esm.js'
@@ -17,7 +21,9 @@ import * as StageManager from './pg-stages-esm.js'
 import * as FxUiPars from './ui/ui-fxpars-esm.js'
 import * as FxUi from './ui/ui-fxpanel-esm.js'
 import * as StagesUi from './ui/ui-stages-esm.js'
+import * as Observer from './ui/ui-observer-esm.js'
 import * as PlayersUi from './ui/ui-players-esm.js'
+//import * as PlayersUiRx from './ui/ui-players-rx-esm.js'
 import * as SourcesUi from './ui/ui-sources-esm.js'
 import * as StatesUi from './ui/ui-states-esm.js'
 
@@ -25,9 +31,12 @@ import { createUI } from './ui/ui-esm.js'
 
 import * as Playground from './playground-esm.js'
 
+//const useReact = window.location.href.includes('react')
+//const PlayersUi = useReact ? PlayersUiRx : PlayersUiNonRx
+
 export {
-  Corelib, Store, DOMplusUltra, DragWithDOM, Midi, TestMidi,
+  Corelib, Store, DOMplusUltra, R, DragWithDOM, Midi, TestMidi,
   onWaapiReady, BeeFX, BPM, Visualizer, createGraphBase,
   Sources, StateManager, StageManager, Playground,
-  StagesUi, FxUiPars, FxUi, PlayersUi, SourcesUi, StatesUi, createUI
+  StagesUi, FxUiPars, FxUi, Observer, PlayersUi, SourcesUi, StatesUi, createUI
 }
